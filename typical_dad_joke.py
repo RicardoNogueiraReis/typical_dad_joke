642639a3e14012356919ac3f6b24000cdfa34b44
Ricardo Nogueira Reis <ricardovreis@gmail.com>
2021-07-15 21:27:26 +0100

    15/07/2021 - An update after a year I first posted this. Now it's possible to use a custom list of words to find in a text (in hindsight, I don't why you would use that for but it's here), reduced code so now instead of removing trailing whitespace manually, the python builtin strip() function is now used. The function now returns text instead of printing it.

--- typical_dad_joke.py
+++ typical_dad_joke.py
@@ -1,60 +1,23 @@
-def dad_joke(text):
+def dad_joke(text, list_of_words=["i'm", "im", "i am", "iam"]):
     """
-    Searches for the word "I'm" or its variants and selects what comes
-    after to form a response (Ex.: "Dad, I'm hungry", "Hello hungry, I'm dad!")
+    Searches for the list_of_words "I'm" or its variants and selects what comes
+    after to form a response (Ex.: "Dad, I'm hungry" -> "Hello hungry, I'm dad!")
+
+    @text String | Contains the variant of the work "I'm"
+    @list_of_words List | Finds them in a text so it can print a response
     """
-    
-    # Variants of the word "I'm"
-    word = ["i'm", "im", "i am", "iam"]
-    
-    # Checks if there is one of the variants present in the text
-    for x in range(len(word)):
-        if word[x].lower() in text.lower():
 
-            lText = text.lower()
-            lWord = word[x]
+    text = text.lower()
 
-            # Finds the word "I'm"
-            wordEndIndex = lText.find(lWord) + len(word[x].lower())
-
-            # Length of the text
-            text_EndIndex = len(text) - 1
-
-            # Removes unnecessary spaces after the word "I'm"
-            while text[wordEndIndex] == " ":
-                wordEndIndex += 1
-
-            # Removes unnecessary spaces at the end of the string
-            while text[text_EndIndex] == " ":
-                text_EndIndex -= 1
-
-            print(f"Hello \"{text[wordEndIndex:].capitalize()}\", I'm Dad!")
-            
-def dad_joke_custom(list_of_words, text):
-    """
-    Searches for a word/list of words customized by the user
-    List_of_words should be a list
-    """
-    
     # Checks if there is one of the variants present in the text
     for x in range(len(list_of_words)):
-        if list_of_words[x].lower() in text.lower():
+        if list_of_words[x].lower() in text:
+            list_of_words[x] = list_of_words[x].lower()
 
-            lText = text.lower()
-            lWord = list_of_words[x]
+            # Finds the word "I'm" or any words specified in the optional parameter
+            word_end_index = text.find(list_of_words[x]) + len(list_of_words[x])
 
-            # Finds the word "I'm"
-            wordEndIndex = lText.find(lWord) + len(word[x].lower())
+            return f"Hello \"{text[word_end_index:].strip().capitalize()}\", I'm Dad!"
 
-            # Length of the text
-            text_EndIndex = len(text) - 1
 
-            # Removes unnecessary spaces after the word "I'm"
-            while text[wordEndIndex] == " ":
-                wordEndIndex += 1
-
-            # Removes unnecessary spaces at the end of the string
-            while text[text_EndIndex] == " ":
-                text_EndIndex -= 1
-
-            print(f"Hello \"{text[wordEndIndex:].capitalize()}\", I'm Dad!")
+# print(text := dad_joke("Dad, I'm hungry"))
